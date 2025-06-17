5d8d8c074a864609e7dea55e8ed52077db8a1486
Aaron Knudtson <aaron.knudtson@outlook.com>
2025-06-17 14:32:26 -0400

    add hyperdx and winston

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import HyperDX from '@hyperdx/browser'
+
+HyperDX.init({
+  apiKey: import.meta.env.VITE_HYPERDX_API_KEY,
+  service: 'example-vue-app',
+  tracePropagationTargets: [/api.myapp.domain/i], // Set to link traces from frontend to backend requests
+  consoleCapture: true, // Capture console logs (default false)
+  advancedNetworkCapture: true, // Capture full HTTP request/response headers and bodies (default false)
+})
 
 createApp(App).mount('#app')
